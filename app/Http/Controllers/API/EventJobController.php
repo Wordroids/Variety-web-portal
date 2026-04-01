4b2bbd0834d01e74958e94aa51ceca0da8b0ea1d
Naseef <AUNaseef@users.noreply.github.com>
2026-04-01 23:46:59 +0530

    Jobs listing

---
+++ app/Http/Controllers/API/EventJobController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+
+class EventJobController extends Controller
+{
+    public function index(Event $event)
+    {
+        return response()->json([
+            "success" => true,
+            "message" => "Jobs listed.",
+            "permits" => $event->jobs,
+        ]);
+    }
+}
